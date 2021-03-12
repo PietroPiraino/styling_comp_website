8340d7d2c6614b6ec3510466b21c4d22fb6c8895
Pietro Piraino <Kondom91@Pietros-MBP.home-life.hub>
2021-03-12 08:14:31 +0100

    commit styled comp

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,112 @@
+import React from "react";
+import {
+  FaFacebook,
+  FaInstagram,
+  FaYoutube,
+  FaTwitter,
+  FaLinkedin,
+} from "react-icons/fa";
+import { Button } from "../../globalStyles";
+import {
+  FooterContainer,
+  FooterSubscription,
+  FooterSubHeading,
+  FooterSubText,
+  Form,
+  FormInput,
+  FooterLinksContainer,
+  FooterLinksWrapper,
+  FooterLinksItems,
+  FooterLinkTitle,
+  FooterLink,
+  SocialMedia,
+  SocialMediaWrap,
+  SocialLogo,
+  WebsiteRights,
+  SocialIcons,
+  SocialIconLink,
+  SocialIcon,
+} from "./Footer.elements";
+
+const Footer = () => {
+  return (
+    <FooterContainer>
+      <FooterSubscription>
+        <FooterSubHeading>
+          Join our exclusive membership to receive the latest news and trends
+        </FooterSubHeading>
+        <FooterSubText>You can subscribe at any time.</FooterSubText>
+        <Form>
+          <FormInput name="email" type="email" placeholder="Your Email" />
+          <Button fontBig>Subscribe</Button>
+        </Form>
+      </FooterSubscription>
+      <FooterLinksContainer>
+        <FooterLinksWrapper>
+          <FooterLinksItems>
+            <FooterLinkTitle>About us</FooterLinkTitle>
+            <FooterLink to="/sign-up">How it works</FooterLink>
+            <FooterLink to="/">Testimonials</FooterLink>
+            <FooterLink to="/">Careers</FooterLink>
+            <FooterLink to="/">Partners</FooterLink>
+            <FooterLink to="/">Terms of service</FooterLink>
+          </FooterLinksItems>
+          <FooterLinksItems>
+            <FooterLinkTitle>Contanct us</FooterLinkTitle>
+            <FooterLink to="/sign-up">How it works</FooterLink>
+            <FooterLink to="/">Testimonials</FooterLink>
+            <FooterLink to="/">Careers</FooterLink>
+            <FooterLink to="/">Partners</FooterLink>
+            <FooterLink to="/">Terms of service</FooterLink>
+          </FooterLinksItems>
+        </FooterLinksWrapper>
+        <FooterLinksWrapper>
+          <FooterLinksItems>
+            <FooterLinkTitle>Videos</FooterLinkTitle>
+            <FooterLink to="/sign-up">How it works</FooterLink>
+            <FooterLink to="/">Testimonials</FooterLink>
+            <FooterLink to="/">Careers</FooterLink>
+            <FooterLink to="/">Partners</FooterLink>
+            <FooterLink to="/">Terms of service</FooterLink>
+          </FooterLinksItems>
+          <FooterLinksItems>
+            <FooterLinkTitle>Social media</FooterLinkTitle>
+            <FooterLink to="/sign-up">How it works</FooterLink>
+            <FooterLink to="/">Testimonials</FooterLink>
+            <FooterLink to="/">Careers</FooterLink>
+            <FooterLink to="/">Partners</FooterLink>
+            <FooterLink to="/">Terms of service</FooterLink>
+          </FooterLinksItems>
+        </FooterLinksWrapper>
+      </FooterLinksContainer>
+      <SocialMedia>
+        <SocialMediaWrap>
+          <SocialLogo to="/">
+            <SocialIcon />
+            Styled Components
+          </SocialLogo>
+          <WebsiteRights>Styled Components &#169; 2021</WebsiteRights>
+          <SocialIcons>
+            <SocialIconLink href="/" target="_blank" aria-label="Facebook">
+              <FaFacebook />
+            </SocialIconLink>
+            <SocialIconLink href="/" target="_blank" aria-label="Instagram">
+              <FaInstagram />
+            </SocialIconLink>
+            <SocialIconLink href="/" target="_blank" aria-label="Youtube">
+              <FaYoutube />
+            </SocialIconLink>
+            <SocialIconLink href="/" target="_blank" aria-label="Twitter">
+              <FaTwitter />
+            </SocialIconLink>
+            <SocialIconLink href="/" target="_blank" aria-label="Linkedin">
+              <FaLinkedin />
+            </SocialIconLink>
+          </SocialIcons>
+        </SocialMediaWrap>
+      </SocialMedia>
+    </FooterContainer>
+  );
+};
+
+export default Footer;
---
+++ src/pages/HomePage/Data.js
@@ -0,0 +1,50 @@
+export const homeObjOne = {
+  lightBg: false,
+  imgStart: "",
+  lightTopLine: true,
+  lightText: true,
+  lightTextDesc: true,
+  buttonLabel: "Get Started",
+  description:
+    "We help software developers style their components in the most efficient way. Our reactivity is the the best in the web!",
+  headline: "Functionality specialist for Front-End devs",
+  topLine: "Development Tools",
+  primary: true,
+  img: require("../../images/img1.svg").default,
+  alt: "Phone",
+  start: "",
+};
+
+export const homeObjTwo = {
+  lightBg: true,
+  imgStart: "start",
+  lightTopLine: false,
+  lightText: false,
+  lightTextDesc: false,
+  buttonLabel: "Read Docs",
+  description:
+    "This method of building and styling components is practicle and reusable. You will definetely do more work in half of the time compared to traditional programming.",
+  headline: "Once you learn you never go back",
+  topLine: "Time saving",
+  primary: false,
+  img: require("../../images/laptopandcoffee.jpg").default,
+  alt: "laptop and coffee",
+  start: "true",
+};
+
+export const homeObjThree = {
+  lightBg: false,
+  imgStart: "",
+  lightTopLine: true,
+  lightText: true,
+  lightTextDesc: true,
+  buttonLabel: "Get Started",
+  description:
+    "No more long nights of work! All the time you save with the styling components can be used to do some sport, read books, hook up and get drunk!",
+  headline: "Functionality specialist for Front-End devs",
+  topLine: "Work smart and be happy",
+  primary: true,
+  img: require("../../images/img2.svg").default,
+  alt: "walking in the park",
+  start: "",
+};
